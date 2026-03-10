e8f2af4c7e5d60035d4284230dc1d08103958b5a
soskutia <andris.soskuti@gmail.com>
2026-03-10 20:59:53 +0100

    before dark theme change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://soskutia.github.io',
-  base: '/dugulassegely-site',
+  output: 'static',
+  trailingSlash: 'never',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
+
+
